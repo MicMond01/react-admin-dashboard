2b5df8e6c91b3884214b45928d11bc6341600dd9
captain <koladeokudero@gmail.com>
2023-03-06 15:02:27 +0100

    admin

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -5,93 +5,102 @@ import MoreVertIcon from "@mui/icons-material/MoreVert";
 import IconButton from "@mui/material/IconButton";
 import { useTheme } from "@emotion/react";
 import { tokens } from "../theme";
+import { Draggable } from "react-beautiful-dnd";
 
 const ContactCard = ({ type, name, telephone, email, address, city, id }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
   return (
-    <Box
-      maxWidth="300px"
-      sx={{
-        bgcolor: colors.redAccent[800],
-        paddingTop: "5px",
-        borderRadius: "10px 10px 0 0",
-      }}
-    >
+    <Draggable>
       <Box
+        maxWidth="280px"
+        mb="10px"
         sx={{
-          bgcolor: colors.grey[800],
-          padding: "15px",
+          bgcolor: colors.redAccent[800],
+          paddingTop: "5px",
           borderRadius: "10px 10px 0 0",
         }}
       >
         <Box
           sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            marginBottom: "5px",
+            bgcolor: colors.grey[800],
+            padding: "15px",
+            borderRadius: "10px 10px 0 0",
           }}
         >
-          <Typography variant="h4" fontWeight={theme.typography.fontWeightBold}>
-            {type}
-          </Typography>
-          <Box>
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          </Box>
-        </Box>
-
-        <Box>
           <Box
             sx={{
-              bgcolor: colors.grey[700],
-              width: "100px",
-              borderRadius: "7px",
-              marginBottom: "10px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              marginBottom: "5px",
             }}
           >
-            <Typography sx={{ color: colors.grey[200], textAlign: "center" }}>
-              {name}
+            <Typography
+              variant="h4"
+              fontWeight={theme.typography.fontWeightBold}
+            >
+              {type}
             </Typography>
+            <Box>
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+            </Box>
           </Box>
-          <Box display="flex" marginBottom="5px">
-            <Typography sx={{ color: colors.grey[400], marginRight: "5px" }}>
-              Telephone:
-            </Typography>
-            <Typography sx={{ color: colors.grey[200] }}>
-              {telephone}
-            </Typography>
-          </Box>
-          <Box display="flex" marginBottom="5px">
-            <Typography sx={{ color: colors.grey[400], marginRight: "5px" }}>
-              Email:
-            </Typography>
-            <Typography sx={{ color: colors.grey[200] }}>{email}</Typography>
-          </Box>
-          <Box display="flex" marginBottom="5px">
-            <Typography sx={{ color: colors.grey[400], marginRight: "5px" }}>
-              Address:
-            </Typography>
-            <Typography sx={{ color: colors.grey[200] }}>{address}</Typography>
-          </Box>
-          <Box display="flex" marginBottom="5px">
-            <Typography sx={{ color: colors.grey[400], marginRight: "5px" }}>
-              City:
-            </Typography>
-            <Typography sx={{ color: colors.grey[200] }}>{city}</Typography>
-          </Box>
-          <Box display="flex" marginBottom="5px">
-            <Typography sx={{ color: colors.grey[400], marginRight: "5px" }}>
-              Register Id:
-            </Typography>
-            <Typography sx={{ color: colors.grey[200] }}>{id}</Typography>
+
+          <Box>
+            <Box
+              sx={{
+                bgcolor: colors.grey[700],
+                width: "150px",
+                borderRadius: "7px",
+                marginBottom: "10px",
+              }}
+            >
+              <Typography sx={{ color: colors.grey[200], textAlign: "center" }}>
+                {name}
+              </Typography>
+            </Box>
+            <Box display="flex" marginBottom="5px">
+              <Typography sx={{ color: colors.grey[400], marginRight: "5px" }}>
+                Telephone:
+              </Typography>
+              <Typography sx={{ color: colors.grey[200] }}>
+                {telephone}
+              </Typography>
+            </Box>
+            <Box display="flex" marginBottom="5px">
+              <Typography sx={{ color: colors.grey[400], marginRight: "5px" }}>
+                Email:
+              </Typography>
+              <Typography sx={{ color: colors.grey[200] }}>{email}</Typography>
+            </Box>
+            <Box display="flex" marginBottom="5px">
+              <Typography sx={{ color: colors.grey[400], marginRight: "5px" }}>
+                Address:
+              </Typography>
+              <Typography sx={{ color: colors.grey[200] }}>
+                {address}
+              </Typography>
+            </Box>
+            <Box display="flex" marginBottom="5px">
+              <Typography sx={{ color: colors.grey[400], marginRight: "5px" }}>
+                City:
+              </Typography>
+              <Typography sx={{ color: colors.grey[200] }}>{city}</Typography>
+            </Box>
+            <Box display="flex" marginBottom="5px">
+              <Typography sx={{ color: colors.grey[400], marginRight: "5px" }}>
+                Register Id:
+              </Typography>
+              <Typography sx={{ color: colors.grey[200] }}>{id}</Typography>
+            </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
+    </Draggable>
   );
 };
 
--- src/scenes/leads/index.jsx
+++ src/scenes/leads/index.jsx
@@ -1,22 +1,239 @@
-import { Box } from "@mui/material";
+// import { Box, List } from "@mui/material";
+// import React, { useState } from "react";
+// import { mockDataContacts } from "../../data/mockData";
+// import ContactCard from "./../../components/ContactCard";
+// import { Draggable } from "react-beautiful-dnd";
+// import { DragDropContext, Droppable } from "react-beautiful-dnd";
+// // import { Draggable } from "@fullcalendar/interaction";
+
+// const reorder = (list, startIndex, endIndex) => {
+//   const result = Array.from(list);
+//   const [removed] = result.splice(startIndex, 1);
+//   result.splice(endIndex, 0, removed);
+//   return result;
+// };
+
+// const Leads = () => {
+//   const [project, setProject] = useState(mockDataContacts);
+
+//   // const onDragEnd = (result) => {
+//   //   if (!result.destination) return;
+//   //   if (result.destination.index === result.source.index) return;
+//   //   const projects = reorder(
+//   //     project, // project is state
+//   //     result.source.index,
+//   //     result.destination.index
+//   //   );
+//   //   //store reordered state.
+//   //   setProject(projects);
+//   // };
+
+//   // onDragEnd={onDragEnd}
+
+//   return (
+//     <DragDropContext>
+//       <Box
+//         m="20px"
+//         style={{
+//           display: "grid",
+//           gridTemplateColumns: "65% 33%",
+//           overflow: "auto",
+//           gap: "20px",
+//           overflowX: "hidden",
+//         }}
+//       >
+//         <Droppable droppableId="list">
+//           <List
+//             style={{
+//               height: "90vh",
+//               width: "100%",
+//               display: "grid",
+//               gap: "5px",
+//               gridTemplateColumns: "50% 50%",
+//               overflow: "auto",
+//               overflowX: "hidden",
+//             }}
+//           >
+//             {project.slice(0, 5).map((item, index) => (
+//               <Draggable index={index} key={item.id}>
+//                 <ContactCard
+//                   type={item.type}
+//                   name={item.name}
+//                   address={item.address}
+//                   telephone={item.phone}
+//                   email={item.email}
+//                   city={item.city}
+//                   id={item.registrarId}
+//                 />
+//               </Draggable>
+//             ))}
+//           </List>
+//         </Droppable>
+//         {/* <List
+//           style={{
+//             maxHeight: "90vh",
+//             width: "100%",
+//             display: "grid",
+//             gap: "5px",
+//             gridTemplateColumns: "1fr",
+//             overflow: "auto",
+//           }}
+//         >
+//           {mockDataContacts.slice(5, 20).map((item) => (
+//             <ContactCard
+//               key={item.id}
+//               type={item.type}
+//               name={item.name}
+//               address={item.address}
+//               telephone={item.phone}
+//               email={item.email}
+//               city={item.city}
+//               id={item.registrarId}
+//             />
+//           ))}
+//         </List> */}
+//       </Box>
+//     </DragDropContext>
+//   );
+// };
+
+// export default Leads;
+
+// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+// import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
 import React from "react";
-import { mockDataContacts } from "../../data/mockData";
-import ContactCard from "./../../components/ContactCard";
+// import { makeStyles } from "@material-ui/core/styles";
+// import TreeView from "@material-ui/lab/TreeView";
+// import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+// import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+// import TreeItem from "@material-ui/lab/TreeItem";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { TreeItem } from "@mui/lab";
+import { TreeView } from "@mui/lab";
 
-const index = () => {
+const datas = [
+  { id: 2, label: "aaa" },
+  { id: 3, label: "bbb" },
+  { id: 4, label: "ccc" },
+];
+
+// const useStyles = makeStyles({
+//   root: {
+//     height: 216,
+//     flexGrow: 1,
+//     maxWidth: 400,
+//   },
+// });
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 250,
+});
+
+export default function Leads() {
+  // const classes = useStyles();
+  const [expanded, setExpanded] = React.useState([]);
+  const [selected, setSelected] = React.useState([]);
+
+  const handleToggle = (event, nodeIds) => {
+    setExpanded(nodeIds);
+  };
+
+  const handleSelect = (event, nodeIds) => {
+    setSelected(nodeIds);
+  };
+
+  const onDragEnd = () => {
+    alert(1);
+  };
+
   return (
-    <Box m="20px">
-      <Box>
-        {mockDataContacts.map((item) => (
-          <ContactCard
-            name={item.name}
-            address={item.address}
-            telephone={item.phone}
-          />
-        ))}
-      </Box>
-    </Box>
+    <DragDropContext>
+      <Droppable droppableId="droppable">
+        {(provided, snapshot) => (
+          <div
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            style={getListStyle(snapshot.isDraggingOver)}
+          >
+            <TreeView
+              sx={{ height: 216, flexGrow: 1, maxWidth: 400 }}
+              defaultCollapseIcon={<ExpandMoreIcon />}
+              defaultExpandIcon={<ChevronRightIcon />}
+              expanded={expanded}
+              selected={selected}
+              onNodeToggle={handleToggle}
+              onNodeSelect={handleSelect}
+            >
+              <TreeItem nodeId="1" label="Applications">
+                {datas.map((data, index) => (
+                  <Draggable draggableId={`${data.id}`} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
+                      >
+                        <TreeItem
+                          key={data.id}
+                          nodeId={data.id}
+                          label={data.label}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </TreeItem>
+            </TreeView>
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
-};
+}
 
-export default index;
+// export default function DndTreeView() {
+//   const classes = useStyles();
+//   const [expanded, setExpanded] = React.useState([]);
+//   const [selected, setSelected] = React.useState([]);
+
+//   const handleToggle = (event, nodeIds) => {
+//     setExpanded(nodeIds);
+//   };
+
+//   const handleSelect = (event, nodeIds) => {
+//     setSelected(nodeIds);
+//   };
+
+//   const onDragEnd = () => {
+//     alert(1);
+//   };
+
+//   return (
+
+//   );
+// }
